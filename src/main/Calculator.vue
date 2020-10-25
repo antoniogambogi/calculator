e50401c24ffdd4b4afbe391c0f700fa63ffa8c15
antoniogambogi <antoniogambogi88@gmail.com>
2020-10-25 03:37:03 -0300

    Implementando funcionalidades no displau

--- src/main/Calculator.vue
+++ src/main/Calculator.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="calculator">
-      <Display />
-      <Button />
+      <Display value="1000"/>
+      <Button label="AC" triple />
+      <Button label="/" operation/>
+      <Button label="7"/>
+      <Button label="8"/>
+      <Button label="9"/>
+      <Button label="*" operation/>
+      <Button label="4"/>
+      <Button label="5"/>
+      <Button label="6"/>
+      <Button label="-" operation/>
+      <Button label="1"/>
+      <Button label="2"/>
+      <Button label="3"/>
+      <Button label="+" operation/>
+      <Button label="0" double/>
+      <Button label="."/>
+      <Button label="=" operation/>
+
   </div>
 </template>
 
@@ -22,7 +39,7 @@ export default {
         overflow: hidden;
 
         display: grid;
-        grid-template-columns: repeat(4,25%);
-        grid-template-rows: 1fr 48px 48px 48px 48px;
+        grid-template-columns: repeat(4, 25%);
+        grid-template-rows: 1fr 48px 48px 48px 48px 48px;
     }
 </style>
